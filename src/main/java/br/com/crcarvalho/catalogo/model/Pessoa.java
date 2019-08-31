80aa37ab374a6f20fb2df7e43fdff9c4c8e8e9d1
Rafael Carvalho <crcarvalho@outlook.com>
2019-08-31 19:17:56 -0300

    Criando as entidades do sistema

---
+++ src/main/java/br/com/crcarvalho/catalogo/model/Pessoa.java
@@ -0,0 +1,54 @@
+package br.com.crcarvalho.catalogo.model;
+
+import java.util.List;
+
+public class Pessoa {
+	
+	private Long id;
+	private String nome;
+	private String cpf;
+	private List<Endereco> enderecos;
+	private List<Telefone> telefones;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+
+	public List<Telefone> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(List<Telefone> telefones) {
+		this.telefones = telefones;
+	}
+
+	
+}
---
+++ src/main/java/br/com/crcarvalho/catalogo/model/Telefone.java
@@ -0,0 +1,33 @@
+package br.com.crcarvalho.catalogo.model;
+
+public class Telefone {
+	
+	private Long id;
+	private String ddd;
+	private String numero;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDdd() {
+		return ddd;
+	}
+
+	public void setDdd(String ddd) {
+		this.ddd = ddd;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+}
